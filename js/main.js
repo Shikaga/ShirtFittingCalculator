3c1b287a61e461a4ac36a0e7178ca43683949423
Shikaga <shikaga@gmail.com>
2014-01-02 00:03:23 +0100

    MVP

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
-function add(number1, number2)
-{
-	return number1 + number2;
+function ValueHandler(identifier, initSize) {
+	this.identifier = identifier;
+	this.circumference = ko.observable(initSize);
+	this.width = ko.observable(-1);
+
+	this.min = ko.observable(1);
+	this.max = ko.observable(100);
+	this.units = ko.observable("inches");
+	this.circumference.subscribe(function(value) {
+		this.calculateWidth();
+		console.log(value);
+	}.bind(this))
+
+	this.calculateWidth();
 }
 
-function sub(number1,number2)
-{
-    return number1 - number2;
+ValueHandler.prototype.calculateWidth = function() {
+	var width = (parseFloat(this.circumference()) + 5) / 2;
+	this.width(width); //inches!
 }
 
-console.log(sub(5,4));
+function ShirtFittingCalculator() {
+	this.exampleArray = ko.observableArray([
+		//21.5", 20.25", 19.25", 19.0", 20", and 21
+		new ValueHandler("Armpits", 43),
+		new ValueHandler("Button 1", 41),
+		new ValueHandler("Button 2", 39),
+		new ValueHandler("Button 3", 37),
+		new ValueHandler("Button 4", 39),
+		new ValueHandler("Button 5", 41),
+		new ValueHandler("Button 6", 41)]);
+	ko.applyBindingsToNode(document.getElementById('example'), null, this);
+}
